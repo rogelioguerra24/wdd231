a2491e23817795d91c6cde97b16e8a7544e630b7
rogelioguerra24 <gue24018@byui.edu>
2025-03-14 18:13:26 -0500

    Sycron images

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,18 @@
+// This part is to give date to the footer
+const today = new Date();
+
+const year = document.querySelector("#currentyear");
+year.innerHTML = `${today.getFullYear()}`;
+
+const lastModified = document.lastModified;
+const last = document.querySelector("#lastModified");
+last.innerHTML = `${lastModified}`;
+
+// This part is to use the hamburguer selector of the page
+const burgerElement = document.querySelector("#myButton");
+const menuElement = document.querySelector("#animated-met");
+
+burgerElement.addEventListener('click', () => {
+    menuElement.classList.toggle('open');
+    burgerElement.classList.toggle('open');
+});
